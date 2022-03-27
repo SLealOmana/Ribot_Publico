1544862ea11d64062e825bf9a7a9880533831a7a
私は朝食が好きです <56454387+SLealOmana@users.noreply.github.com>
2022-03-27 20:02:15 +0000

    s

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def check_queue(ctx,arg):
     
 def youtube(search):
   query_string = parse.urlencode({'search_query':search})
-  html_content = request.urlopen('http://www.youtube.com/results?'+ query_string)
+  html_content = request.urlopen('https://www.youtube.com/results?'+ query_string)
   search_results = re.findall( r"watch\?v=(\S{11})", html_content.read().decode())
   return "https://www.youtube.com/watch?v="+search_results[0]
 @bot.command()
